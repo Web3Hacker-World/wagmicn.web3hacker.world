f5b20cf263949cfffbab6b08c59d130966aad1ca
Web3HackerWorld <web3hackerdao@gmail.com>
2023-05-30 13:57:07 +0800

    🚚 rename to use core folder

--- scripts/syncSource.mjs
+++ scripts/syncSource.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env zx
 
-const destDir = path.join(__dirname, `../content/en/`)
+const destDir = path.join(__dirname, `../content/en/core/`)
 const sourceDir = path.join(__dirname, `../../_github/wagmi/docs/pages/core/`)
 
 await $`cd ${sourceDir} && git pull`
-await $`cp -r ${sourceDir}/** ${destDir}`
+await $`mkdir -p ${destDir} && cp -r ${sourceDir}/** ${destDir}`
 
